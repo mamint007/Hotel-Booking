e688da300075d091ae1e3d7c8f3a052606c68f0f
Kasitinard <yelaysong15@gmail.com>
2025-12-22 22:48:29 +0700

    fix UI Font

--- apps/hotel-app/src/pages/_app.tsx
+++ apps/hotel-app/src/pages/_app.tsx
@@ -1,24 +1,6 @@
-// pages/_app.tsx
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Poppins } from "next/font/google";
-import { createGlobalStyle } from "styled-components";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: var(--font-poppins), sans-serif;
-  }
-`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={poppins.className}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />;
 }
